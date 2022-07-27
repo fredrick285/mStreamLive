b867335c8e003e5860e5026a3fbaccae2435c8bc
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-07-27 03:39:34 +0300

    draft

---
+++ my-movie-app/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(){
+
+    return(
+        <div className="about">
+        <h2>About Us</h2>
+        <p>We provide you with the HD movies</p>
+        </div>
+    )
+}
+export default About;
